c1044a9639cc32beb31ef7249c52269351b7e062
preetisha08 <preetisha5120@gmail.com>
2026-08-07 19:17:35 +0530

    Time: 23 ms (28.07%), Space: 59.9 MB (12.7%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class RecentCounter {
+
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+
+        // Add current request
+        queue.offer(t);
+
+        // Remove requests outside the 3000ms window
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
