3f7184d94c3794d919e5af6de1a93e5fe5543d49
preetisha08 <preetisha5120@gmail.com>
2026-08-17 19:37:15 +0530

    Time: 0 ms (100%), Space: 43.2 MB (52.59%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+
+        List<Integer> leaves1 = new ArrayList<>();
+        List<Integer> leaves2 = new ArrayList<>();
+
+        getLeaves(root1, leaves1);
+        getLeaves(root2, leaves2);
+
+        return leaves1.equals(leaves2);
+    }
+
+    private void getLeaves(TreeNode root, List<Integer> leaves) {
+
+        if (root == null) {
+            return;
+        }
+
+        // Leaf node
+        if (root.left == null && root.right == null) {
+            leaves.add(root.val);
+            return;
+        }
+
+        getLeaves(root.left, leaves);
+        getLeaves(root.right, leaves);
+    }
+}
